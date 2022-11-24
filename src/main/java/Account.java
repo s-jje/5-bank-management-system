074c1f5cae56d44a112d87542fda92db93a4e692
skyriv213 <gn1007@dankook.ac.kr>
2022-11-24 11:28:52 +0900

    계좌 정보 /  거래내역을 불러오는 toString 부분 작성

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,3 +1,7 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 
@@ -5,10 +9,11 @@ public class Account {
 
     private UUID uuid;
     private String name;
+
     private String accountNumber;
+
     private long balance;
     private List<TransactionalInfo> transactionalInfoList;
-
     public Account(UUID uuid, String name, String accountNumber, long balance, List<TransactionalInfo> transactionalInfos) {
         this.uuid = uuid;
         this.name = name;
@@ -29,8 +34,37 @@ public class Account {
         return 0;
     }
 
-    public List<TransactionalInfo> getAllTranscationalInfos() {
+    public UUID getUuid() {
+        return uuid;
+    }
 
-        return null;
+    public String getName() {
+        return name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    /***
+     * 계좌내의 TransactionalInfo 객체를 불러와서 toString 메서드 실행
+     * @return searchTransactionalInfo List
+     */
+    public void getAllTranscationalInfos() {
+        for (TransactionalInfo transactionalInfo : this.transactionalInfoList) {
+            System.out.println(transactionalInfo.toString());
+        }
+    }
+
+    /***
+     * 계좌 내의 정보 출력을 위한 메서드
+     */
+    @Override
+    public String toString() {
+        return "Account{" +
+                "name='" + name + '\'' +
+                ", accountNumber='" + accountNumber + '\'' +
+                ", balance=" + balance +
+                '}';
     }
 }
--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,3 +1,5 @@
+package main.java;
+
 import java.util.UUID;
 
 public class Customer {
