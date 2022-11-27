651ec482373548632176e1c7197ee94d67d18ecd
woojeong <yang965273@gmail.com>
2022-11-28 05:54:44 +0900

    register() 계좌번호 정규표현식, 계좌생성 시 비밀번호 일치여부 판단

--- src/main/java/bank/ShinhanBank.java
+++ src/main/java/bank/ShinhanBank.java
@@ -1,6 +1,10 @@
 package bank;
 
+import bankaccount.ShinhanBankAccount;
+import useraccount.UserAccount;
+
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class ShinhanBank extends Bank {
 
@@ -20,19 +24,38 @@ public class ShinhanBank extends Bank {
     @Override
     public void register() {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter your Name: ");
+        System.out.printf("%n");
+        System.out.print("이름을 입력해 주세요: ");
         String name = scanner.nextLine();
-
-        System.out.println("Enter your Id: ");
+        System.out.print("사용하실 ID를 입력해 주세요: ");
         String id = scanner.nextLine();
 
-        System.out.println("Enter your Password: ");
+        System.out.print("사용하실 PASSWORD를 입력해 주세요: ");
         String pw = scanner.nextLine();
+        System.out.print("한 번 더 입력해 주세요: ");
+        String pwCheck = scanner.nextLine();
+
+        // 비밀번호 일치하는지 확인
+        if (!pw.equals(pwCheck)) {
+            System.out.println("비밀번호가 일치하지 않습니다.");
+            return;
+        }
+
+        // 계좌번호 정규표현식으로 입력
+        String pattern = "110-\\d{3}-\\d{6}";
+        System.out.println("사용하실 계좌번호를 입력해 주세요");
+        System.out.print("형식) 110-xxx-xxxxxx : ");
+        String accountNumber = scanner.next();
+        if(!Pattern.matches(pattern,accountNumber)){
+            System.out.println("잘못된 형식입니다.");
+            return ;
+        }
 
         // 계좌번호 랜덤 생성
-        String accountNumber = "110-" + (int) ((Math.random() * 999) + 1) + "-" + (int) ((Math.random() * 999999) + 1);
+//        String accountNumber = "110-" + (int) ((Math.random() * 999) + 1) + "-" + (int) ((Math.random() * 999999) + 1);
 
+        getBankAccountList().add(new ShinhanBankAccount(name, id, pw, getName(), accountNumber, 0L));
+        getCustomerList().add(new UserAccount(name, id, pw));
         // 계좌번호 출력
         System.out.printf("Account registration successful! Account Number is %s%n%n", accountNumber);
     }
