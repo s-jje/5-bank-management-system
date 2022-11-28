f2de5921bd18e402117c72ac20ac4c7b5734ca7e
sssssjj <sshsjhome@gmail.com>
2022-11-28 16:28:41 +0900

    refactor

--- src/main/java/bankaccount/TransactionData.java
+++ src/main/java/bankaccount/TransactionData.java
@@ -12,15 +12,6 @@ public class TransactionData {
     private final long balance;
     private final String description;
 
-    public TransactionData(String accountNumber, boolean isDeposit, long amount, long balance, String description) {
-        this.date = TimeFormatter.format(Time.getCurrentDateTime());
-        this.accountNumber = accountNumber;
-        this.isDeposit = isDeposit;
-        this.amount = amount;
-        this.balance = balance;
-        this.description = description;
-    }
-
     public TransactionData(String date, String accountNumber, boolean isDeposit, long amount, long balance, String description) {
         this.date = date;
         this.accountNumber = accountNumber;
