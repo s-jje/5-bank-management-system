d6bd38ac32b04e18ddaf1b8d419f339273aac938
woojeong <yang965273@gmail.com>
2022-11-28 10:29:40 +0900

    코드 정리

--- src/main/java/bankaccount/ShinhanBankAccount.java
+++ src/main/java/bankaccount/ShinhanBankAccount.java
@@ -1,8 +1,5 @@
 package bankaccount;
 
-import util.MoneyFormatter;
-
-import java.util.Scanner;
 
 public class ShinhanBankAccount extends BankAccount {
 
@@ -12,26 +9,11 @@ public class ShinhanBankAccount extends BankAccount {
 
     @Override
     public void deposit() {
-        System.out.print("Please enter the amount you want to deposit: ");
-        Scanner scanner = new Scanner(System.in);
-        long amount = Long.parseLong(scanner.nextLine());
 
-        if (amount > 0) {
-            long balance = getBalance() + amount;
-            setBalance(balance);
-            addTransactionData(new TransactionData(getAccountNumber(), true, amount, balance, "Shinhan Bank"));
-            System.out.println("Deposit successful!");
-            System.out.printf("Your balance is ₩%s%n", MoneyFormatter.formatToWon(balance));
-        } else {
-            System.out.println("You can deposit more than ₩0.");
-        }
     }
 
     @Override
     public void withdrawal() {
-        System.out.print("Please enter the amount you want to withdrawal: ");
-        Scanner scanner = new Scanner(System.in);
-        long amount = Long.parseLong(scanner.nextLine());
 
     }
 
